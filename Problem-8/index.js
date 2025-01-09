d3efdf679788a1cd488e8c5ce865915176b7eeb8
Sahil Wadaskar <wadaskarsahil718@gmail.com>
2025-01-09 13:43:12 +0530

    Function Transformations

---
+++ Problem-8/index.js
@@ -0,0 +1,25 @@
+/**
+    2629. Function Composition
+ 
+    Given a list of functions, compose the functions together to create a single function.
+ */
+
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    
+    return function(x) {
+        result = x;
+        for (let i= functions.length-1; i>= 0; i--){
+            result = functions[i](result);
+        }
+        return result;
+    };
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
